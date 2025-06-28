1d9842e7e6f047e78b3e61ab48f1b7091ba3a015
Tycoon-Learning <tycoong2111@gmail.com>
2025-06-28 11:55:44 +0530

    Add files via upload

---
+++ Assignment 2/Odd even.py
@@ -0,0 +1,5 @@
+Number= int(input("\nEnter a number: "))
+if Number % 2 == 0:
+    print(f"{Number} is an Even Number")
+else:
+    print(f"{Number} is an Odd Number")
---
+++ Assignment 2/Sum of Numbers (Loop).py
@@ -0,0 +1,6 @@
+total = 0
+Number= 1
+while Number<=50:
+    total+=Number
+    Number+=1
+print("The sum of Number from 1 to 50 is:", total)
